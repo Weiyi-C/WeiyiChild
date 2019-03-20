f68d69aed370b0bdb5d730eb5b83893724d3bea7
郑维一 <weiyi@zhengweiyi.cn>
2019-03-20 18:07:44 +0800

    修复 安全接送页面打开闪退
    添加 安全接送页面返回按钮点击事件

--- app/src/main/java/cn/zhengweiyi/weiyichild/PickupActivity.java
+++ app/src/main/java/cn/zhengweiyi/weiyichild/PickupActivity.java
@@ -8,6 +8,7 @@ package cn.zhengweiyi.weiyichild;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import java.util.Objects;
 
@@ -18,11 +19,18 @@ public class PickupActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pickup);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar_pickup);
         setSupportActionBar(toolbar);
         //设置是否有返回箭头
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
         StatusBarUtil.setStatusBarMode(this, false, R.color.themeDark);
     }
