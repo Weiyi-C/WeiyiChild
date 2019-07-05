f9d79690a0a7ea95427d141e4fd8ddd7ab6e110a
郑维一 <weiyi@zhengweiyi.cn>
2019-07-05 15:41:06 +0800

    添加 编辑食谱内容页相关文件
    添加 编辑食谱fragment布局中加入两个底部按钮
    添加 编辑食谱fragment获取和显示食谱的逻辑
    添加 编辑食谱item的布局
    添加 编辑食谱的Recycler适配器
    添加 编辑食谱的按钮布局
    添加 在资源文件中添加相应数据
    
    注意：编辑食谱fragment尚存在下拉应用内重启问题

--- app/src/main/java/cn/zhengweiyi/weiyichild/fragment/DietaryEditFragment.java
+++ app/src/main/java/cn/zhengweiyi/weiyichild/fragment/DietaryEditFragment.java
@@ -7,18 +7,38 @@ package cn.zhengweiyi.weiyichild.fragment;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+
+import cn.zhengweiyi.weiyichild.MyApplication;
 import cn.zhengweiyi.weiyichild.R;
+import cn.zhengweiyi.weiyichild.bean.Dietary;
+import cn.zhengweiyi.weiyichild.custom.DateFormatUtil;
+import cn.zhengweiyi.weiyichild.custom.DietaryRecyclerEditAdapter;
+import cn.zhengweiyi.weiyichild.greenDao.DietaryLab;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class DietaryEditFragment extends Fragment {
 
+    private List<Dietary> dietaryList;
+    private RecyclerView mRecyclerView;
+    private MyApplication app;
+    private DietaryLab dietaryLab;
+    private DietaryRecyclerEditAdapter adapter;
+
+    private String selectDate;
 
     public DietaryEditFragment() {
         // Required empty public constructor
@@ -26,10 +46,34 @@ public class DietaryEditFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dietary_edit, container, false);
+        View view = inflater.inflate(R.layout.fragment_dietary_edit, container, false);
+        // 获取Application
+        app = (MyApplication) Objects.requireNonNull(getActivity()).getApplication();
+        // 获取食谱
+        dietaryLab = new DietaryLab(app.getDaoSession().getDietaryDao());
+        dietaryList = dietaryLab.getDietaryByDate(DateFormatUtil.DateToStr(new Date()));
+        // 显示食谱
+        mRecyclerView = view.findViewById(R.id.dietary_edit_recycler);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(layoutManager);
+        return view;
+    }
+
+    public boolean isScrollTop() {
+        return mRecyclerView != null && mRecyclerView.computeVerticalScrollOffset() == 0;
+    }
+
+    public void changeDate(String date) {
+        selectDate = date;
+        if (dietaryList.size() > 0) {
+            dietaryList.clear();
+        }
+        dietaryList = dietaryLab.getDietaryByDate(date);
+        Log.d("DietaryRecycler", date + "的新食谱列表大小为" + dietaryList.size());
+        adapter.refreshData(dietaryList);
     }
 
 }
