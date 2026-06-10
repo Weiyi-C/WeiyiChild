ad998c36e102b1c229da4a395c9edd29f822be59
郑维一 <weiyi@zhengweiyi.cn>
2026-06-11 00:15:19 +0800

    迁移 SettingsActivity 和 AppCompatPreferenceActivity 到 AndroidX

--- app/src/main/java/cn/zhengweiyi/weiyichild/SettingsActivity.java
+++ app/src/main/java/cn/zhengweiyi/weiyichild/SettingsActivity.java
@@ -20,14 +20,14 @@ import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
-import android.support.v7.app.ActionBar;
+import androidx.appcompat.app.ActionBar;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.preference.RingtonePreference;
 import android.text.TextUtils;
 import android.util.DisplayMetrics;
 import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+import androidx.core.app.NavUtils;
 
 import java.util.List;
 import java.util.Locale;
